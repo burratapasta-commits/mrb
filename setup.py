352aebb8414d23c2f779d5a7a80f50dfbd6550b5
burratapasta-commits <burratapasta@gmail.com>
2026-08-05 04:08:54 +0300

    Add setup.py for package configuration

---
+++ setup.py
@@ -0,0 +1,9 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name='discord-reporter-stealth',
+    version='1.0.0',
+    install_requires=['aiohttp>=3.8.0'],
+    python_requires='>=3.8'
+)
