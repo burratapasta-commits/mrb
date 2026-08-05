1fc94588157d2223e8432662ad8e9c936b8a3d81
burratapasta-commits <burratapasta@gmail.com>
2026-08-05 04:35:42 +0300

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-# setup.py
-from setuptools import setup
-
-setup(
-    name='discord-reporter-stealth',
-    version='1.0.0',
-    install_requires=['aiohttp>=3.8.0'],
-    python_requires='>=3.8'
-)
