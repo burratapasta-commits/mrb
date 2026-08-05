93beb730800991c7725ce71964909431049c47fc
burratapasta-commits <burratapasta@gmail.com>
2026-08-05 04:36:02 +0300

    Delete install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# install.sh
-echo "[+] Installing Discord Stealth Reporter"
-pkg update -y
-pkg install python -y
-pip install aiohttp
-chmod +x main.py
-echo "[+] Done. Run: python main.py"
